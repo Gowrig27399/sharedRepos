ed54bcff522d5251d4d231c4b3e38ea9314bc295
Gowrig27399 <gowrig27399@gmail.com>
2021-02-08 20:19:58 +0530

    first commit

---
+++ sharedproject/src/sharedproject/SamplClassEx.java
@@ -0,0 +1,17 @@
+package sharedproject;
+
+public class SamplClassEx {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			System.out.println("adding 2 number");
+			int a=5,b=7;
+			int c=a+b;
+			System.out.println("c:"+c);
+		}
+
+	}
+
+
