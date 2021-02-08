520ebe6e6f38d5b6f31b7a83a18fe4e348e60cbb
Gowrig27399 <gowrig27399@gmail.com>
2021-02-08 16:56:30 +0530

    first commit

---
+++ sharedproject/src/sharedproject/sampleclass.java
@@ -0,0 +1,14 @@
+package sharedproject;
+
+public class sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("adding 2 number");
+		int a=5,b=7;
+		int c=a+b;
+		System.out.println("c:"+c);
+	}
+
+}
